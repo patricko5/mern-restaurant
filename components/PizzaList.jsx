c65be744ba8438c3c883ec4241ca33d91c100fcb
patricko5 <64801567+patricko5@users.noreply.github.com>
2022-10-18 12:14:52 -0600

    Footer commit 2

---
+++ components/PizzaList.jsx
@@ -0,0 +1,31 @@
+import styles from "../styles/PizzaList.module.css";
+import PizzaCard from "./PizzaCard";
+
+const PizzaList = () => {
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>
+        {" "}
+        THE UNCOMPARABLE PIZZA FROM SOUTH AB.
+      </h1>
+      <p className={styles.desc}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
+        mollitia, molestiae quas vel sint commodi repudiandae consequuntur
+        voluptatum laborum numquam blanditiis harum quisquam eius sed odit
+        fugiat iusto fuga praesentium optio, eaque rerum!
+      </p>
+      <div className={styles.wrapper}>
+        <PizzaCard />
+        <PizzaCard />
+        <PizzaCard />
+        <PizzaCard />
+        <PizzaCard />
+        <PizzaCard />
+        <PizzaCard />
+        <PizzaCard />
+      </div>
+    </div>
+  );
+};
+
+export default PizzaList;
