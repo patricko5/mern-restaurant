6debc15ea0f3897394b6a67faa2a0eb217e958a2
Patrick Orihuela <patrick.orihuela@gmail.com>
2022-10-27 15:59:18 -0600

    Time:  1:20:00

--- pages/orders/[id].jsx
+++ pages/orders/[id].jsx
@@ -1,10 +1,66 @@
-import styles from "../../styles/Order.module.css"
-import Image from "next/image"
+import styles from "../../styles/Order.module.css";
+import Image from "next/image";
 
 const Order = () => {
   return (
-    <div>Order</div>
-  )
-}
+    <div className={styles.container}>
+      <div className={styles.left}>
+        <div className={styles.row}>
+          <table className={styles.table}>
+            <tbody>
+              {/*It cause hydration errors if it is not inside a tbody  */}
+              <tr className={styles.tr}>
+                <th>Order ID</th>
+                <th>Customer</th>
+                <th>Address</th>
+                <th>Total</th>
+              </tr>
+              <tr className={styles.tr}>
+                <td>
+                  <span className={styles.id}>8650198724</span>
+                </td>
+                <td>
+                  <span className={styles.name}>Patrick Orihuela</span>
+                </td>
+                <td>
+                  <span className={styles.address}>4123 78 Ave NW</span>
+                </td>
+                <td>
+                  <span className={styles.total}>$39.80</span>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        <div className={styles.row}>
+          <div className={styles.status}>
+            <Image src="/img/paid.png" width={30} height={30} alt="" />
+            <span>Payment</span>
+            <div className={styles.checkIcon}>
+            <Image src="/img/checked.png" width={30} height={30} alt="" />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className={styles.right}>
+        <div className={styles.wrapper}>
+          <h2 className={styles.title}>CART TOTAL</h2>
+          <div className={styles.totalText}>
+            <b className={styles.totalTextTitle}>Subtotal:</b>$79.60
+          </div>
+          <div className={styles.totalText}>
+            <b className={styles.totalTextTitle}>Discount:</b>$0.00
+          </div>
+          <div className={styles.totalText}>
+            <b className={styles.totalTextTitle}>Total:</b>$79.60
+          </div>
+          <button disabled className={styles.button}>
+            PAID
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Order
+export default Order;
