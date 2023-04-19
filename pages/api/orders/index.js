0842cb2790867d3ce96dda5fa7b5ef404b6ad35c
Patrick O <patrick.orihuela@gmail.com>
2023-04-18 22:48:00 -0600

    POST api/orders route enabled

--- pages/api/orders/index.js
+++ pages/api/orders/index.js
@@ -15,9 +15,6 @@ const handler = async (req, res) => {
     }
   }
   if (method === "POST") {
-    if (!token || token !== process.env.TOKEN) {
-      return res.status(401).json("Not Authenticated!");
-    }
     try {
       const order = await Order.create(req.body);
       res.status(201).json(order);
